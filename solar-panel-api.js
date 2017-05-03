d23ef697593d527baa9af8708d2e76ede1459cc3
kianacramer <kiana.cramer@aol.com>
2017-05-03 14:48:55 -0400

    Update solar-panel-api.js

--- solar-panel-api.js
+++ solar-panel-api.js
@@ -1,33 +1,24 @@
-var request = require('request-promise'); 
+var request = require('request-promise');
 
-api_key = 'hUeKIgQuZMkhyIP0MR8pAZ2Ea5HYAt5HuHVff345' 
-api_url = 'https://developer.nrel.gov/api/pvwatts/v5.json' 
+api_key = 'hUeKIgQuZMkhyIP0MR8pAZ2Ea5HYAt5HuHVff345'
+api_url = 'https://developer.nrel.gov/api/pvwatts/v5.json'
 
 module.exports = {
-	
-	solarPanelDataRequest: function(userLocation){
-		var parameters = {'api_key': api_key, 'address': userLocation, 'system_capacity': 4, 'azimuth': 180, 
-					'tilt': 40, 'array_type': 1, 'module_type': 1, 'losses': 10, 'radius': 0, 'timeframe': 'hourly'}			
+	solarPanelDataRequest: function(locationGiven) {
+		var parameters = {'api_key': api_key, 'address': locationGiven, 'system_capacity': 4, 'azimuth': 180,
+					'tilt': 40, 'array_type': 1, 'module_type': 1, 'losses': 10, 'radius': 0, 'timeframe': 'hourly'}
 
 		return request({url: api_url, qs: parameters})
 	},
-
-	powerForMonth: function(month, solarData){
-		   dateIndex = new Date(Date.parse(month + " 1, 2012")).getMonth() 
-		   return solarData.outputs.ac_monthly[dateIndex]
+	powerForMonth: function(month, responseData){
+		   dateIndex = new Date(Date.parse(month + " 1, 2012")).getMonth()
+		   return responseData.outputs.ac_monthly[dateIndex]
 	},
 
-	powerForDate: function(date, solarData){
-		   dateIndex = new Date(Date.parse(date)).getMonth() 
+	powerForDate: function(date, responseData){
+		   dateIndex = new Date(Date.parse(date)).getMonth()
 		   console.log(dateIndex)
-		   return solarData.outputs.ac_monthly[dateIndex]
+		   return responseData.outputs.ac_monthly[dateIndex]
 	}
 
 }
-
-
-	
-
-
-
-
